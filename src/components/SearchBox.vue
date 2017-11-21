819dd1ceb9d221d953c03f1ad25a31520027adc7
yinyan-0.3.min.js <i@yinyan.fr>
2017-11-21 17:19:49 +0100

    fix test

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="is-component is-searchbox">
         <div class="is-icon is-searchbox" ref='icon' v-on:click='FocusOn("input")'></div>
-        <input class='is-field is-searchbox' type='text' ref='input' v-model='entry' />
+        <input class='is-field is-searchbox id-searchbox' type='text' ref='input' v-model='entry' />
     </div>
 </template>
 
