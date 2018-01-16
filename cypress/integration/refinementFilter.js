37e87c282d33c02704b7543a393f0488df6fad1e
TrimA <florianseb96@hotmail.fr>
2018-01-16 11:06:42 +0100

    added new test for RefinementListFilter
    created a context for filtered research
    
    testing Hits results

--- cypress/integration/refinementFilter.js
+++ cypress/integration/refinementFilter.js
@@ -1,6 +1,14 @@
 const _URL = 'http://localhost:4000/#/test_refinementListFilter',
       _REFINEMENT_LIST_FILTER = '.is-component.is-refinement-list',
-      _HITS = '.is-score.is-hits';
+      _HITS = '.is-score.is-hits',
+      _ITEMS = 'div.hit',
+
+      _FIRSTNAME = '.firstname',
+      _LASTNAME = '.lastname',
+      _STATE = '.state',
+      _GENDER = '.gender';
+
+import refinementFilterList_sample_1 from '../fixtures/refinementFilterList_1.json';
 
 describe('Test RefinementListFilter', () => {
   beforeEach(function() {
@@ -16,8 +24,26 @@ describe('Test RefinementListFilter', () => {
       expect($el.get(0)).to.have.prop("checked",true);
     })
   });
-  it('should be filtering as expected' , function () {
-    cy.get(_REFINEMENT_LIST_FILTER + ' input:first').check();
-    cy.get(_REFINEMENT_LIST_FILTER + ' label:first').contains('tx ( 30 )');
+  context('Filtered research', function () {
+    const valCheck = {
+      name : 'md',
+      count : 28
+    };
+    beforeEach(function () {
+      cy.get(_REFINEMENT_LIST_FILTER + ' input').check(valCheck.name);
+    });
+    it('should be filtering as expected' , function () {
+      cy.get(_REFINEMENT_LIST_FILTER + ' label:first').contains(valCheck.name+' ( '+valCheck.count+' )');
+    });
+    it('Hits results for filtering by md state', function() {
+      cy.get(_ITEMS).each((item, index) => {
+        let _currentHit = refinementFilterList_sample_1.hits.hits[index]._source;
+        cy.wrap(item).find(_FIRSTNAME).contains(_currentHit.firstname);
+        cy.wrap(item).find(_LASTNAME).contains(_currentHit.lastname);
+        cy.wrap(item).find(_STATE).contains(_currentHit.state);
+        cy.wrap(item).find(_GENDER).contains(_currentHit.gender);
+      });
+    });
   });
+
 });
