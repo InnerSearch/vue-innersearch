3dc65459572af8c7bba15b690257909a664b0460
TrimA <florianseb96@hotmail.fr>
2018-07-28 16:31:12 +0200

    ♻ moved the template at his correct location

--- examples/Restaurants.vue
+++ examples/Restaurants.vue
@@ -1,12 +1,31 @@
 <template>
-    <div id='defaultForm'>
-    </div>
+    <section>
+        <h1 class='is-title'>innerSearch.js</h1>
+
+        <searchbox :field="'Dba'" :realtime="true"></searchbox>
+
+        <map-hits :mapKey="GOOGLE_MAP_KEY">
+            <template slot="hits" slot-scope="{ hits }">
+                <div class="is-score is-hits">
+                    <strong v-if="hits.score === 0">No result found</strong>
+                    <strong v-else-if="hits.score === 1">1 result found</strong>
+                    <strong v-else-if="hits.score > 1">{{ hits.score }} results found</strong>
+                </div>
+                <div v-for="(item,key) in hits.items" :key="key">
+                    <div  style="margin : 10px;">
+                        <p><strong>NOM :</strong> {{ item._source.Dba }}</p>
+                        <p><strong>Action :</strong> {{ item._source.Action }}</p>
+                        <p><strong>Zipcode :</strong> {{ item._source.Zipcode }}</p>
+                    </div>
+                </div>
+            </template>
+        </map-hits>
+
+        <hr class='is-line' />
+    </section>
 </template>
 
 <script>
-    /*
-        Simple way to use InnerSearch
-    */
 
     import Vue from 'vue';
     import InnerSearch from "../src/innerSearch.js";
@@ -15,48 +34,18 @@
 
     Vue.use(InnerSearch);
 
-    window.addEventListener('load', function () {
-        new Vue({
-            el: '#defaultForm',
-            components : {
-                'map-hits' : MapHits
-            },
+    export default {
+        
+        components : {
+            'map-hits' : MapHits
+        },
 
-            created : function () {
-                // ES server configuration
-                this.setHost('http://innersearch:' + process.env.ELASTIC_INNERSEARCH_PASSWORD + '@elastic.innersearch.tk/');
-                this.setIndex('nyc_restaurants');
-                this.GOOGLE_MAP_KEY = process.env.GOOGLE_MAP_KEY;
-            },
+        created : function () {
+            // ES server configuration
+            this.setHost('http://innersearch:' + process.env.ELASTIC_INNERSEARCH_PASSWORD + '@elastic.innersearch.tk/');
+            this.setIndex('nyc_restaurants');
+            this.GOOGLE_MAP_KEY = process.env.GOOGLE_MAP_KEY;
+        },
+    }
 
-            template : `
-                <section>
-                    <h1 class='is-title'>innerSearch.js</h1>
-
-                    <searchbox :field="'Dba'" :realtime="true"></searchbox>
-
-                    <map-hits :mapKey="GOOGLE_MAP_KEY">
-                        <template slot="hits" slot-scope="{ hits }">
-                            <div class="is-score is-hits">
-                                <strong v-if="hits.score === 0">No result found</strong>
-                                <strong v-else-if="hits.score === 1">1 result found</strong>
-                                <strong v-else-if="hits.score > 1">{{ hits.score }} results found</strong>
-                            </div>
-                            <div v-for="item in hits.items" :item="item">
-                                <div style="margin : 10px;">
-                                    <p><strong>NOM :</strong> {{ item._source.Dba }}</p>
-                                    <p><strong>Action :</strong> {{ item._source.Action }}</p>
-                                    <p><strong>Zipcode :</strong> {{ item._source.Zipcode }}</p>
-                                </div>
-                            </div>
-                        </template>
-                    </map-hits>
-
-                    <hr class='is-line' />
-                </section>
-            `
-        });
-    });
-
-    export default {};
 </script>
